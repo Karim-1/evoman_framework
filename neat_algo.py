5a1ca8fd6cfc409f8706dcdf5d851b5c04183cf7
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-09-25 16:35:50 +0200

    small plot function (will be changed)

--- neat_algo.py
+++ neat_algo.py
@@ -8,6 +8,7 @@
 # imports framework & libraries
 import sys, os
 import neat
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
@@ -18,21 +19,30 @@ from controller import Controller
 from demo_controller import NEAT_controller
 from environment import Environment
 
+# TODO:
+# --> may be good to crossect between individuals that are most different
+# --> use around 25-150 generations (common in literature), roughly 2-5 changes per generation (depends on pop size)
+
 
 def eval_genomes(genomes, config):
     '''
     evaluates the fitness of each genome
+    and stores the best and mean fitness
     '''
+    genome_fitnesses = []
     for genome_id, genome in genomes:
-        fitness, plife, elife, sim_time = simulate(genome)
+        fitness, p, e, t = simulate(genome)
+
         genome.fitness = fitness
-        print('\n\n genome fitness=',genome.fitness)
+        genome_fitnesses.append(fitness)
+    
+    # retrieve mean and best fitness for the genomes
+    mean_fitness.append(np.mean(genome_fitnesses))
+    best_fitness.append(np.max(genome_fitnesses))
+
 
     return genomes
     
-    
-# --> may be good to crossect between individuals that are most different
-# --> use around 25-150 generations (common in literature), roughly 2-5 changes per generation (depends on pop size)
 
 def simulate(genome):
     return env.play(pcont=genome)
@@ -45,30 +55,37 @@ def run(path, experiment_name):
                         neat.DefaultSpeciesSet, 
                         neat.DefaultStagnation,
                         config_file)
-    
-    # create random NEAT population
+    # create NEAT population
     pop = neat.Population(config)
     
+    # add reporter for population
     pop.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     pop.add_reporter(stats)
     pop.add_reporter(neat.Checkpointer(5))
     
-    winner = pop.run(eval_genomes,50)
+    t0 = time.time()
+
+    # run all generations and pick winner
+    winner = pop.run(eval_genomes,10)
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
-    
-    t0 = time.time()
-    
-    
     
     ''' stop condition --> reaching some minimum level of diversity?
     or --> reaching some specified number of generations without fitness improvement '''
     
     t1 = time.time()
+
+    plt.scatter(range(1, len(mean_fitness)+1, 1), mean_fitness, label='Mean genome fitness')
+    plt.scatter(range(1, len(best_fitness)+1, 1), best_fitness, label='Best genome fitness')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.show()
     
-    print(f'\n----- simulation took {t1-t0} seconds -----\n')
+    print(f'\n----- simulation took {np.round(t1-t0,1)} seconds -----\n')
+    print(mean_fitness)
+    print(best_fitness)
 
 
 if __name__ == "__main__":
@@ -81,6 +98,9 @@ if __name__ == "__main__":
     if experiment_name == 'neat':
         config_file = os.path.join(local_dir, "neat_config.txt")
 
+    # remove game display
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
     # initialize environment with NEAT network as player controller
     env = Environment(
         experiment_name=experiment_name,
@@ -92,4 +112,10 @@ if __name__ == "__main__":
         level=2,
         speed='fastest'
         )
+
+    # lists to store information for each generation
+    mean_fitness = []
+    best_fitness = []
+
+    # run experiments
     run(config_file, experiment_name)
